3fc311309007a65dddc9cc1bfa41ed3fc0c9a1e6
Ughur Huseynli <u.hub.programming@gmail.com>
2024-04-16 17:16:00 +0400

    request body

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class TestModel(BaseModel):
+    name: str
+    surname: str
+    age: int
+    email: str
+    description: str | None = None
 
 class ModelName(str, Enum):
     patient = 'patient'
@@ -20,3 +29,20 @@ def read_file(file_path: str):
 @app.get('/items/{item_id}')
 def home(item_id: int):
     return {'message': f'hello world { item_id }'}
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.post('/testmodel')
+def pydanticTest(item: TestModel):
+    return {'item': item}
